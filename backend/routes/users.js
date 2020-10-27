1b5de827d5a332f742dc87b622aa413f3cd79829
Kiarash Zamanifar <kiarashz@hotmail.com>
2020-10-27 23:28:11 +1100

    set ports

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // res.send('respond with a resource');
   res.json([
     {id: 1, username: "kiarash"},
     {id: 2, username: "reza"}
