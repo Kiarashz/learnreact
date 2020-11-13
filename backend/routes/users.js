2f796b3d0c19e3dcde8c987b0dcfd7be7b7cfd2a
Kiarash Zamanifar <kiarashz@hotmail.com>
2020-11-13 19:05:57 +1100

    path to api

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  if (req.headers.referer !== process.env.UI_URL) {
+    res.status(401).send("You are not authorized to access this endpoint")
+  }
   res.json([
     {id: 1, username: "kiarash"},
     {id: 2, username: "reza"}
